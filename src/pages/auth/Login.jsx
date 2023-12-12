902f796317fe8184e06b8b37709ea0bc7ae97747
Marco <of.mukesh@gmail.com>
2023-12-12 18:49:17 +0530

    login min screen

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <>
-      <section className="bg-gray-50 dark:bg-gray-900">
+      <section className="bg-gray-50 dark:bg-gray-900 min-h-screen">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <a
             href="#"
