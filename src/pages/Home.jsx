937933f25f2fe0d8dbd255559ed19cb4a1ac4cd7
Marco <of.mukesh@gmail.com>
2023-12-26 23:22:52 +0530

    Home uodated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      Home
+      <div className="text-center p-8">
+        <h1 className="text-4xl font-medium my-8">Generate NOCs with Ease</h1>
+        <p className="my-6">
+          The NOC Generator is a user-friendly tool designed to help you create
+          No Objection Certificates (NOCs) quickly and efficiently. Whether you
+          need an NOC for an internship, job placement, or any other purpose,
+          our platform simplifies the process for you.
+        </p>
+        <button className="rounded bg-gray-800 text-white p-2">
+          Learn more
+        </button>
+      </div>
     </>
   );
 };
